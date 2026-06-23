6da70ac550204251ec8ab6900dc483f44e28f7c0
Lemarboks <Lemarboks@users.noreply.github.com>
2026-06-23 01:04:23 +0000

    deploy: a9222eaf1fa71c007c7a012191bfe35a11802739

---
+++ _next/static/782ZIoH8DYaRM3kgKsz6K/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/virtual_home_builder//_next/:path+",
+        "destination": "/virtual_home_builder/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
